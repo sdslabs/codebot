d53ac698be1f656ae1b6c61060912c95fa3424ca
Abhay Rana <capt.n3m0@gmail.com>
2013-09-23 22:17:55 +0530

    Updated the problems to include <p> tags

--- problems/html2md.php
+++ problems/html2md.php
@@ -5,7 +5,7 @@ foreach(glob("html/*") as $file)
 	echo $file."\n";
 	$html = file_get_html($file);
 	$problem = $html->find(".problem_content",0);
-	$problem = strip_tags($problem,"<br>,<img>");
+	$problem = strip_tags($problem,"<br>,<img>,<p>");
 	$file_name = basename($file,".html").".md";
 	file_put_contents($file_name, $problem);
 }
