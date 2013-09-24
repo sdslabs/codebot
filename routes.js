981c8034cceb4102c4f04a35e8441e97e614dd7a
Abhay Rana <capt.n3m0@gmail.com>
2013-09-24 23:11:44 +0530

    Added lots of easy routes. Now we support a fake fs, and some basic commands. Also, some easter eggs/

--- routes.js
+++ routes.js
@@ -17,7 +17,7 @@ module.exports=function(app,config){
     var html = problems.getHTML(id);
     if(!html)
     {
-      res.json([{msg:"No such problem",className:"error"}]);
+      res.json("[[;;;red]No such problem]");
     }
     else
     {
@@ -38,7 +38,7 @@ module.exports=function(app,config){
    */
   app.get('/help',function(req,res){
     res.json([
-      { msg:"SDSLabs Codematics is a program to help you discover & solve cool mathematical problems. You can type the following commands: "},
+      { raw: true, msg:"SDSLabs CodeBot is an alternative interface to <a href='https://projecteuler.net'>Project Euler</a>. You can type the following commands: "},
       { msg:"[[b;;;white]help] For this help message"},
       { msg:'[[b;;;white]problems] to see first 10 problems' },
       { msg:'[[b;;;white]problems <start> <end>] to view a list of problems numbering from start to end.' },
@@ -49,6 +49,8 @@ module.exports=function(app,config){
       { msg:'Several other terminal commands (like [[;;;red]ls,cd,whoami] etc) are also supported.' }
     ]);
   });
+
+  /** Problem related routes */
   /** Get a list of Problems */
   app.get('/problems',function(req,res){
     res.redirect('/problems/1/10');
@@ -61,14 +63,114 @@ module.exports=function(app,config){
   app.get('/problem/:start/:end', function(req,res){
     res.redirect('/problems/'+req.params.start+"/"+req.params.end);
   });
+  //Show the problems in a table
   app.get('/problems/:start/:end', function(req,res){
     var start=parseInt(req.params.start), end=parseInt(req.params.end);
     response = ["Showing problems "+start+" to "+end];
     for(var i=start;i<=end;i++){
       if(i>430)
         break;
-      response.push(util.pad(i.toString(),3,' ',1)+". "+problems.getTitle(i));
+      response.push("[[b;;;]"+util.pad(i.toString(),3,' ',1)+".] "+problems.getTitle(i));
     }
     res.json(response);
+  });
+
+  /** Session aware routes **/
+  app.get('/pwd',function(req,res){
+    if(!req.session.cwd)
+      req.session.cwd = '/';
+    res.json(req.session.cwd);
+  });
+
+  app.get('/ls',function(req,res){
+    var cwd = req.session.cwd||'/';
+    switch(cwd){
+      case 'problems':
+        res.redirect('/problems');
+        break;
+      case 'users':
+        res.json('all top recent');
+        break;
+      default:
+        res.json('[[b;;;]problems/]  README [[b;;;]users/]');
+    }
+  });
+
+  app.get(/(file|open|cat|more|less)\/(\S+)/,function(req,res){
+    var cwd = req.session.cwd || '/';
+    var cmd=req.params[0];
+    var file = req.params[1];
+    switch(cwd){
+      case '/':
+        if(file == 'README')
+          res.json({msg: '<b>CodeBot</b> is a mathematical initiative by <a target="_blank" href="https://facebook.com/SDSLabs">SDSLabs</a>, IIT Roorkee. The problems are same as those of <a href="https://projecteuler.net">Project Euler</a> and are used under a <a href="https://projecteuler.net/copyright">CC BY-NC-SA 2.0 UK</a> Licence.', raw:true});
+        else
+          res.json("cat: "+file+": No such file or directory");
+        break;
+      case 'problems':
+        res.redirect('/problem/'+file);
+        break;
+      case 'users':
+        res.redirect('/user/'+file);
+        break;
+      default:
+        res.json(cmd+": "+file+": No such file or directory");
+    }
+  });
+
+  app.get('/cd/:dir?',function(req,res){
+    var cwd = req.session.cwd;
+    var dir = req.params.dir || '/';
+    if(dir == '/' || dir == 'users' || dir == 'problems')
+    {
+      req.session.cwd = dir//change directory
+      res.redirect('/pwd');//notify user of directory change
+    }
+    else
+      res.json("[[;;;red]Invalid Directory]");
+      //invalid directory
+  });
+
+  app.get('/apt-get/moo',function(req,res){
+    res.json([
+      {raw:true,msg:"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(__)"},
+      {raw:true,msg:"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(oo)"},
+      {raw:true,msg:"&nbsp;&nbsp;&nbsp;/------\/&nbsp;"},
+      {raw:true,msg:"&nbsp;&nbsp;/&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;||&nbsp;&nbsp;"},
+      {raw:true,msg:"&nbsp;*&nbsp;&nbsp;/\\---/\\&nbsp;"},
+      {raw:true,msg:"&nbsp;&nbsp;&nbsp;&nbsp;~~&nbsp;&nbsp;&nbsp;~~&nbsp;"},
+      {raw:true,msg:"....\"Have&nbsp;you&nbsp;mooed&nbsp;today?\"..."}
+    ]);
+  });
+  app.get('/moo',function(req,res){
+    res.json("Have you mooed today?");
+  });
+  app.get('/sudo/*',function(req,res){
+    res.json('Make me a sandwich');
+  });
+  app.get('/sv_restart/1',function(req,res){
+    res.json("Try running your own server if you wanna restart it.");
+  });
+  app.get('/google/:word',function(req,res){
+    res.json('@@Google@@ says '+req.params.word+' does not exist');
+  });
+  app.get('/*siri*',function(req,res){
+    res.json('No, I\'m not Siri, thankyou');
+  });
+  app.get('*', function(req, res){
+    var str = require('querystring').unescape(req.params[0]).replace(/\//g,' ');
+    if(str.charAt(str.length-1)=='?'){
+      //we get to ask wolfram alpha!
+      /**
+      wa.query(str,function(result){
+        if(typeof result == "string")
+          res.json({msg:result});
+        else
+          res.json({msg:"I did not understand that question.",className:"error"});
+      });
+      **/
+    }
+    else
+      res.json('[[;;;red]'+str+": command not found]");
   });
 }
