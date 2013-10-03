96b2f70ef1ec2109f490d80d21ba4fc061a03037
Abhishek Kandoi <abhikandoi2000@gmail.com>
2013-10-03 22:49:32 +0530

    Added user specific prompt

--- public/main.js
+++ public/main.js
@@ -1,44 +1,58 @@
 $(document).ready(function(){
-  $("#console").terminal(
-    function(command, term){
-      var write = function(data){
-        if($.isArray(data)){
-          for(i in data){
-            write(data[i]);
+  var user = "guest";
+  $.getJSON("/whoami", function(data){
+    user = data;
+    $("#console").terminal(
+      function(command, term){
+        var write = function(data){
+          if($.isArray(data)){
+            for(i in data){
+              write(data[i]);
+            }
+          }
+          else if(typeof data=='string'){
+            term.echo(data, {raw: false});
+          }
+          else if(typeof data=="object"){
+            term.echo(data.msg,{raw:data.raw});
           }
         }
-        else if(typeof data=='string'){
-          term.echo(data, {raw: false});
-        }
-        else if(typeof data=="object"){
-          term.echo(data.msg,{raw:data.raw});
-        }
-      }
-      term.pause();//pause it till we get a reply
-      var url = "/"+command.split(' ').join('/');
-      
-      if(url=="/cd/.."||url=="/cd//" || url=="..")
-        url = "/cd";
-      $.getJSON(url, function(data){
-        write(data);
-        term.resume();
-      });
-      $(document).ajaxError(function(e){
-        term.resume();
-      })
-    },
-    {
-      greetings: 'Welcome to CodeBot. Type [[b;;]help] to begin',
-      height: $(window).height()-100,
-      prompt: 'CodeBot > ',
-      tabcompletion: true,
-      completion: function(term, partialCommand, cb){
-        cb(["problems, problem"]);
+        term.pause();//pause it till we get a reply
+        var url = "/"+command.split(' ').join('/');
+
+        if(url=="/cd/.."||url=="/cd//" || url=="..")
+          url = "/cd";
+        $.getJSON(url, function(data){
+          write(data);
+          //update the user name after register or login
+          if(url.search("/register")==0||url.search("/login")==0){
+            $.getJSON("/whoami", function(data) {
+              term.set_prompt(data + '@CodeBot > ', function(){});
+            });
+            term.resume();
+          } else{
+            term.resume();
+          }
+        });
+
+
+        $(document).ajaxError(function(e){
+          term.resume();
+        })
       },
-      keydown: function(e, term){
-        if(e.keyCode==76 && e.ctrlKey==true)
-          term.clear();
+      {
+        greetings: 'Welcome to CodeBot. Type [[b;;]help] to begin',
+        height: $(window).height()-100,
+        prompt: user + '@CodeBot > ',
+        tabcompletion: true,
+        completion: function(term, partialCommand, cb){
+          cb(["problems, problem"]);
+        },
+        keydown: function(e, term){
+          if(e.keyCode==76 && e.ctrlKey==true)
+            term.clear();
+        }
       }
-    }
-  )
+    );
+  });
 });
